1036b2cefebdc4dd7eba52c2f9670f325716d1df
Libin Seban <libinseban97@gmail.com>
2024-12-31 00:15:15 +0530

    Remove package-lock.json and package.json from the root directory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react';
-import theme from './theme';  // Import your custom theme
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import { Outlet } from 'react-router-dom';
 
 const App = () => {
   return (
-    <ChakraProvider theme={theme}>  {/* Use the custom theme if you have one */}
+    <ChakraProvider>  
       <Box minHeight="100vh" display="flex" flexDirection="column">
         <Navbar />
         <Box flex="1">
